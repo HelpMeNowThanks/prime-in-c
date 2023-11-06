db6270b8b67631dd12ecbad0bfcec536f2b345d6
HelpMeNowThanks <67766329+HelpMeNowThanks@users.noreply.github.com>
2023-11-06 23:05:32 +0100

    Add files via upload

---
+++ primev2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int isPrime(unsigned long long int num) {
+	if(num<=1) {return 0;} // 1 and lower aren't primes
+	unsigned long int sqrtnum = ceil(sqrt(num));
+	for(unsigned long long int i = 2; i<=sqrtnum; i++) {
+		if(num==i) {
+			return 1;
+		}
+		if(num%i==0) { //loop through
+			printf("%u\n", i);
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main(int argc, char** argv) {
+	clock_t begin = clock();
+	char* ptr;
+	unsigned long long int num = strtoull(argv[1], &ptr, 10);
+	num = isPrime(num);
+	if(num==1) {
+		printf("PRIME\n");
+	}
+	if(num==0) {
+		printf("NOT PRIME\n");
+	}
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("%.6lf", time_spent);
+}
