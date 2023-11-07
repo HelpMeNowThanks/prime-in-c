a047f886a2ff3632b23ecd4ff6363a7ce99d4300
Makka Pakka <67766329+HelpMeNowThanks@users.noreply.github.com>
2023-11-07 21:04:05 +0100

    Add files via upload

---
+++ primev1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int isPrime(unsigned long long int num) {
+	if(num<=1) {return 0;}
+	for(unsigned long long int i = 2; i<num; i++) {
+		if(num%i==0) {
+			printf("%llu\n", num);
+			printf("%u\n", i);
+			printf("%llu\n", num%i);
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main(int argc, char** argv) {
+	clock_t begin = clock();
+	char* ptr;
+	unsigned long long int num = strtoull(argv[1], &ptr, 10);
+	num = isPrime(num);
+	if(num==1) {
+		printf("PRIME\n");
+	}
+	if(num==0) {
+		printf("NOT PRIME\n");
+	}
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("%lf.4", time_spent);
+}
